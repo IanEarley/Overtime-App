fe9aecc89eba361bc74f79cc5f2d03337dcfed79
Ian Earley <iearley88@gmail.com>
2017-07-12 13:59:17 -0600

    Installed pundit gem for permission structure

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -1,23 +1,33 @@
 require 'rails_helper'
 
 describe 'navigate' do
-    before do
-        @admin_user = FactoryGirl.create(:admin_user)
-        login_as(@admin_user, :scope => :user)
-    end
+  before do
+      @admin_user = FactoryGirl.create(:admin_user)
+      login_as(@admin_user, :scope => :user)
+  end
 
-    describe 'edit' do
+  describe 'edit' do
     before do
       @post = FactoryGirl.create(:post)
     end
 
-   it 'has a status that can be edited on the form' do
-        visit edit_post_path(@post)
+    it 'has a status that can be edited on the form by an admin' do
+      visit edit_post_path(@post)
 
-     choose('post_status_approved')
+      choose('post_status_approved')
       click_on "Save"
 
-     expect(@post.reload.status).to eq('approved')
+      expect(@post.reload.status).to eq('approved')
+    end
+
+    it 'cannot be edited by a non admin' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      visit edit_post_path(@post)
+
+      expect(current_path).to_not have_content('Approved')
     end
   end
 end
