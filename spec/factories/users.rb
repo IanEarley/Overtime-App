0f9ddc4a2fd3d6ef6a4ad0f99cfe7046988d1b81
Ian Earley <iearley88@gmail.com>
2017-07-12 15:44:55 -0600

    Implemented tests and page redirection for permission structure

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,4 +18,12 @@ FactoryGirl.define do
     password 'asdfasdf'
     password_confirmation 'asdfasdf'
   end
+
+  factory :non_authorized_user, class: "User" do
+    first_name 'Bad Jimmy'
+    last_name 'Not Jim-Jim'
+    email { generate :email }
+    password 'asdfasdf'
+    password_confirmation 'asdfasdf'
+  end
 end
