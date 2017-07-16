2f5e59bbc070965408754fb1cb38a894045f607f
Ian Earley <iearley88@gmail.com>
2017-07-16 14:10:24 -0600

    Added overtime hours to index and added validation for valid hour requests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 describe 'navigate' do
   let(:user) { FactoryGirl.create(:user) }
+
   let(:post) do
-    Post.create(date: Date.today, rationale: "Rationale", user_id: user.id)
+    Post.create(date: Date.today, rationale: "Rationale", user_id: user.id, overtime_request: 3.5)
   end
+
   before do
     login_as(user, :scope => :user)
   end
 
-  describe 'Index' do
+  describe 'index' do
     before do
       visit posts_path
     end
@@ -30,9 +32,8 @@ describe 'navigate' do
     end
 
     it 'has a scope so that only post creators can see their posts' do
-
       other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
-      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id, overtime_request: 3.5)
 
       visit posts_path
 
@@ -52,10 +53,11 @@ describe 'navigate' do
   describe 'delete' do
     it 'can be deleted' do
       logout(:user)
+
       delete_user = FactoryGirl.create(:user)
       login_as(delete_user, :scope => :user)
 
-      post_to_delete = Post.create(date: Date.today, rationale: "asdf", user_id: delete_user.id)
+      post_to_delete = Post.create(date: Date.today, rationale: 'asdf', user_id: delete_user.id, overtime_request: 3.5)
 
       visit posts_path
 
@@ -64,34 +66,34 @@ describe 'navigate' do
     end
   end
 
-  describe 'Creation' do
+  describe 'creation' do
     before do
       visit new_post_path
     end
-    
+
     it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
 
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: 'Some rationale'
-      click_on 'Save'
+      fill_in 'post[rationale]', with: "Some rationale"
+      fill_in 'post[overtime_request]', with: 4.5
 
-      expect(page).to have_content('Some rationale')
+      expect { click_on "Save" }.to change(Post, :count).by(1)
     end
 
-    it 'will have a user associated with it' do
+    it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: 'User Association'
-      click_on 'Save'
+      fill_in 'post[rationale]', with: "User Association"
+      fill_in 'post[overtime_request]', with: 4.5
+      click_on "Save"
 
       expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 
   describe 'edit' do
-
     it 'can be edited' do
       visit edit_post_path(post)
 
