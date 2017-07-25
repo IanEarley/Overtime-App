8cf9b89cb4bc1ef3483e16f644665607747f6b26
Ian Earley <iearley88@gmail.com>
2017-07-25 15:31:31 -0600

    Built out rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-@user = User.create(email: "test@test.com", 
+@employee = Employee.create(email: "test@test.com", 
                     password: "123456", 
                     password_confirmation: "123456", 
                     first_name: "Jimmy", 
                     last_name: "Jim-Jim",
                     phone: "8019495782")
 
-puts "1 user created"
+puts "1 employee created"
 
 AdminUser.create(email: "admin@test.com", 
                  password: "asdfasdf", 
@@ -16,20 +16,20 @@ AdminUser.create(email: "admin@test.com",
 
 puts "1 Admin User created" 
 
-AuditLog.create!(user_id: @user.id,
+AuditLog.create!(user_id: @employee.id,
                 status: 0,
                 start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id,
+AuditLog.create!(user_id: @employee.id,
                 status: 0,
                 start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id,
+AuditLog.create!(user_id: @employee.id,
                 status: 0,
                 start_date: (Date.today - 20.days))
 
 puts "3 Audit Logs have been created."
 
 100.times do |post|
-  Post.create(date: Date.today, rationale: "Praesent neque soluta integer vehicula, sollicitudin parturient at interdum rutrum eius maecenas, sed libero malesuada vulputate, dignissim magna, habitant enim volutpat tincidunt quisque a. Vestibulum mattis augue in amet lacus maecenas. Neque integer sit. A mattis lobortis adipiscing tellus. Eu vitae, vel quis nullam vitae vivamus, natoque arcu tortor neque erat nunc tristique. Ullamcorper rutrum vivamus aliquam neque, tellus ullamcorper sed quis ullamcorper, cubilia id purus mi quisque a, a malesuada tincidunt hac, proin eu consequat ac. Et non hac maecenas pede et, vel amet consequat pede augue hendrerit id, ornare gravida vel velit vestibulum donec non.", user_id: @user.id, overtime_request: 2.5)
+  Post.create(date: Date.today, rationale: "Praesent neque soluta integer vehicula, sollicitudin parturient at interdum rutrum eius maecenas, sed libero malesuada vulputate, dignissim magna, habitant enim volutpat tincidunt quisque a. Vestibulum mattis augue in amet lacus maecenas. Neque integer sit. A mattis lobortis adipiscing tellus. Eu vitae, vel quis nullam vitae vivamus, natoque arcu tortor neque erat nunc tristique. Ullamcorper rutrum vivamus aliquam neque, tellus ullamcorper sed quis ullamcorper, cubilia id purus mi quisque a, a malesuada tincidunt hac, proin eu consequat ac. Et non hac maecenas pede et, vel amet consequat pede augue hendrerit id, ornare gravida vel velit vestibulum donec non.", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created."
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -2,8 +2,12 @@ namespace :notification do
   desc "Sends SMS notification to employees asking them to log whether or not they had overtime"
   task sms: :environment do
     if Time.now.sunday?
-    # User.all.each do |user|
-    #   SmsTool.send_sms()
+      employees = Employee.all
+      notification_message = "Please log into the overtime management dashboard to request overtime or confirm your hours for last week: https://ian-overtime.herokuapp.com"
+
+      employees.each do |employee|
+        SmsTool.send_sms(number: employee.phone, message: notification_message)
+      end
     end
   end
 
