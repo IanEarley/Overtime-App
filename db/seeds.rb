5bffba61c73fa82a28ea8e5a7c94b5190106592b
Ian Earley <iearley88@gmail.com>
2017-07-25 14:02:23 -0600

    adjusted load order for seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,6 @@ AdminUser.create(email: "admin@test.com",
 
 puts "1 Admin User created" 
 
-100.times do |post|
-  Post.create(date: Date.today, rationale: "Praesent neque soluta integer vehicula, sollicitudin parturient at interdum rutrum eius maecenas, sed libero malesuada vulputate, dignissim magna, habitant enim volutpat tincidunt quisque a. Vestibulum mattis augue in amet lacus maecenas. Neque integer sit. A mattis lobortis adipiscing tellus. Eu vitae, vel quis nullam vitae vivamus, natoque arcu tortor neque erat nunc tristique. Ullamcorper rutrum vivamus aliquam neque, tellus ullamcorper sed quis ullamcorper, cubilia id purus mi quisque a, a malesuada tincidunt hac, proin eu consequat ac. Et non hac maecenas pede et, vel amet consequat pede augue hendrerit id, ornare gravida vel velit vestibulum donec non.", user_id: @user.id, overtime_request: 2.5)
-end
-
-puts "100 posts have been created."
-
 AuditLog.create!(user_id: @user.id,
                 status: 0,
                 start_date: (Date.today - 6.days))
@@ -33,3 +27,9 @@ AuditLog.create!(user_id: @user.id,
                 start_date: (Date.today - 20.days))
 
 puts "3 Audit Logs have been created."
+
+100.times do |post|
+  Post.create(date: Date.today, rationale: "Praesent neque soluta integer vehicula, sollicitudin parturient at interdum rutrum eius maecenas, sed libero malesuada vulputate, dignissim magna, habitant enim volutpat tincidunt quisque a. Vestibulum mattis augue in amet lacus maecenas. Neque integer sit. A mattis lobortis adipiscing tellus. Eu vitae, vel quis nullam vitae vivamus, natoque arcu tortor neque erat nunc tristique. Ullamcorper rutrum vivamus aliquam neque, tellus ullamcorper sed quis ullamcorper, cubilia id purus mi quisque a, a malesuada tincidunt hac, proin eu consequat ac. Et non hac maecenas pede et, vel amet consequat pede augue hendrerit id, ornare gravida vel velit vestibulum donec non.", user_id: @user.id, overtime_request: 2.5)
+end
+
+puts "100 posts have been created."
